00df18ceeea3ca68a62870cfec5338e1e253fb78
Francesco Canessa <makevoid@gmail.com>
2013-08-13 16:52:43 +0200

    moving forward by reading Cura and CuraEngine code

--- 3dshop/spec/slicer_spec.rb
+++ 3dshop/spec/slicer_spec.rb
@@ -3,15 +3,16 @@ PATH = File.expand_path "../../", __FILE__
 
 class Slicer
 
-  def initialize(file)
+  def initialize(file, settings=nil)
     @file = file
+    @settings = settings
   end
 
 
   def slice
-
     settings = "-s layerThickness=690 -s filamentDiameter=3000 -s filamentFlow=10 -s extrusionWidth=1000"
-
+    settings = @settings if settings
+    
     slicer = "/Applications/Cura/Cura.app/Contents/Resources/CuraEngine"
     output = "#{PATH}/public/gcodes/#{@file}.gcode"
     input  = "#{PATH}/tmp/#{@file}.stl"
@@ -32,31 +33,62 @@ end
 
 describe "Slicer" do
 
-def match_property(out, property)
-  prop_name = property.to_s.gsub(/_/, " ").capitalize
-  match = out.match(/#{prop_name}: (.+)\n/)
-  match[1].to_i if match
-end
+  def match_property(out, property)
+    prop_name = property.to_s.gsub(/_/, " ").capitalize
+    match = out.match(/#{prop_name}: (.+)\n/)
+    match[1].to_i if match
+  end
+
+  def slicer
+    file   = "hollow_cylinder"
+    #  -s extrusionWidth=500 # almost extruder size
+    settings = "-s layerThickness=500 -s filamentDiameter=3000 -s extrusionWidth=500 -s filamentFlow=100 -s printSpeed=100 -s infillSpeed=100 -s moveSpeed=100"
+    out = Slicer.new(file, settings).slice
+    out.should =~ /Filament/
+    out
+  end
+  
+  let(:slice) do
+    @@slice ||= slicer
+  end
+
+  it "has the correct amount of layers" do
+    out = slice
+    match_property(out, :layer_count).should == 20
+  end
+  
+  it "uses the right amount of filament" do
+    out = slice
+    match_property(out, :filament).should == 730 # mm
+  end  
+  
+  it "prints in the right time" do
+    out = slice
+    match_property(out, :print_time).should == 120 # seconds
+  end
+    
+  
+  # it "slices an STL model " do
+  #   file   = "hollow_cylinder"
+  #   settings = "-s layerThickness=500 -s filamentDiameter=300 -s filamentFlow=100 -s extrusionWidth=50 -s printSpeed=1000 -s infillSpeed=10000 -s moveSpeed=10000"
+  #   out = Slicer.new(file, settings).slice
+  #   out.should =~ /Filament/ 
+  #   
+  # 
+  #   match_property(out, :layer_count).should == 20
+  #   match_property(out, :print_time).should == 34
+  #   match_property(out, :filament).should == 73 
+  # end
 
 
-it "slices an STL model " do
-  file   = "hollow_cylinder"
-  out = Slicer.new(file).slice
-  out.should =~ /Filament/ 
-  match_property(out, :filament).should == 160
-  match_property(out, :print_time).should == "..." 
+  # it "fails gracefully reporting the error"
 
-end
-
-
-# it "fails gracefully reporting the error"
-
-xit "slices a different STL model " do
-  file   = "blah"
-  out = Slicer.new(file).slice
-  out.should =~ /Filament/ 
-  match_property(out, :filament).should == 647
-end
+  # xit "slices a different STL model " do
+  #   file   = "blah"
+  #   out = Slicer.new(file).slice
+  #   out.should =~ /Filament/ 
+  #   match_property(out, :filament).should == 647
+  # end
 
 end
 
