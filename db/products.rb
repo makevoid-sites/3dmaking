7278ec40d168fbbc3b11fc0591fa9895cb63b53c
peppe <peppe.dimarzo@gmail.com>
2014-03-04 16:05:32 +0100

    definito format prodotti

--- db/products.rb
+++ db/products.rb
@@ -1,7 +1,30 @@
 
 PRODUCTS = [
-  {  name: :vslot, specs: "1kg", original_price: 0, price: 0, description: "Vslot profiles" },
-  {  name: :assembled_extruder, specs: "2kg", original_price: 0, price: 0, description: "" },    {  name: :linear_bearings, specs: nil, original_price: 0, price: 0, description: "" },
+  { name: :vslot, title: "Vslot 20x20", title_it: "Vslot 20x20",  specs: "1mt black vslot 20x20", original_price: 8, price: 13, description: "Vslot profiles", description_it: "Profili a V in alluminio anodizzato nero", image: 1 },
+  { name: :, title: "", title_it: "",  specs: "", original_price: 0, price: 0, description: "", description_it: "", image: 2 },
+  { name: :, title: "", title_it: "",  specs: "", original_price: 0, price: 0, description: "", description_it: "", image: 2 },
+  { name: :, title: "", title_it: "",  specs: "", original_price: 0, price: 0, description: "", description_it: "", image: 2 },
+  { name: :, title: "", title_it: "",  specs: "", original_price: 0, price: 0, description: "", description_it: "", image: 2 },
+  { name: :, title: "", title_it: "",  specs: "", original_price: 0, price: 0, description: "", description_it: "", image: 2 },
+  { name: :, title: "", title_it: "",  specs: "", original_price: 0, price: 0, description: "", description_it: "", image: 2 },
+  { name: :, title: "", title_it: "",  specs: "", original_price: 0, price: 0, description: "", description_it: "", image: 2 },
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  {  name: :assembled_extruder_jhead, specs: "2kg", original_price: 0, price: 0, description: "" },    
+  {  name: :linear_bearings, specs: nil, original_price: 0, price: 0, description: "" },
   {  name: :belt_set1, specs: nil, original_price: 0, price: 0, description: "" },
   {  name: :belt_set2, specs: "with pulleys", original_price: 0, price: 0, description: "" },
   {  name: :z_couplers, specs: "with pulleys", original_price: 0, price: 0, description: "" },
