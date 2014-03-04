ced8d6dbecc5a4ed00548207b8963fa4bec4e3f4
Francesco Canessa <makevoid@gmail.com>
2014-03-04 15:57:24 +0100

    add db products

---
+++ db/products.rb
@@ -0,0 +1,19 @@
+
+PRODUCTS = [
+  {  name: :vslot, specs: "1kg", original_price: 0, price: 0, description: "Vslot profiles" },
+  {  name: :assembled_extruder, specs: "2kg", original_price: 0, price: 0, description: "" },    {  name: :linear_bearings, specs: nil, original_price: 0, price: 0, description: "" },
+  {  name: :belt_set1, specs: nil, original_price: 0, price: 0, description: "" },
+  {  name: :belt_set2, specs: "with pulleys", original_price: 0, price: 0, description: "" },
+  {  name: :z_couplers, specs: "with pulleys", original_price: 0, price: 0, description: "" },
+  {  name: :axis_transmission_set_3, specs: nil, original_price: 0, price: 0, description: "" },
+  
+  # oggetti stampati 3d
+  {  name: :axis_transmission_set_3, specs: nil, original_price: 0, price: 0, description: "" },
+  
+  
+]
+
+
+# get "/shop" do
+#   haml :products
+# end
